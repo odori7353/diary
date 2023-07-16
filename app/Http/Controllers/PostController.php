e67c090fd4f5c82c412a0bb209963030aef0d1af
odori7353 <fudpen2@gmail.com>
2023-07-16 23:18:28 +0900

    投稿作成画面作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Http\Requests\PostRequest; // useする
+
 
 class PostController extends Controller
 {
@@ -18,5 +20,17 @@ class PostController extends Controller
         {
             return view('posts.show')->with(['post' => $post]);
          //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+        }
+        
+        public function create()
+        {
+            return view('posts.create');
+        }
+        
+        public function store(PostRequest $request, Post $post)
+        {
+            $input = $request['post'];
+            $post->fill($input)->save();
+            return redirect('/posts/' . $post->id);
         }
     }
